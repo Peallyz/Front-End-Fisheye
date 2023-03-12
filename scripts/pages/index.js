f73ed2ea647a73a52bd736a2a4e7f9c68d96c32d
Peally <pieclandre@gmail.com>
2023-03-12 18:47:50 +0100

    doc : add comment on functions

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,13 +1,12 @@
 import { photographerFactory } from "../factories/photographer.js";
 
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  // fetch photographers' data
   let photographers = await fetch("./data/photographers.json")
     .then((response) => response.json())
     .then((data) => data.photographers);
 
-  // et bien retourner le tableau photographers seulement une fois récupéré
+  // return photographers' data
   return {
     photographers,
   };
