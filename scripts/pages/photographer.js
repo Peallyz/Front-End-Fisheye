6bf05eb447c7984665de89e705ff8a6790e8456e
Peally <pieclandre@gmail.com>
2023-03-12 20:52:26 +0100

    feat : display photographer data on their page using id params

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,47 @@
+import { photographerFactory } from "../factories/photographer.js";
+
 //Retrieve current URL id
 
 const url = new URL(window.location);
-const id = url.searchParams.get("id");
+const id = parseInt(url.searchParams.get("id"));
+
+async function getPhotographerData() {
+  // fetch photographers' data
+
+  const getMedia = (medias) => {
+    let mediasArr = medias.filter((media) => media.photographerId === id);
+    return mediasArr;
+  };
+  const getPhotographer = (photographer) => {
+    let photographerData = photographer.filter((data) => data.id === id);
+    return photographerData;
+  };
+
+  const data = await fetch("./data/photographers.json")
+    .then((response) => response.json())
+    .then((data) => [
+      getPhotographer(data.photographers),
+      getMedia(data.media),
+    ]);
+
+  return data;
+}
+
+// Display photographer data on their page
+
+async function displayPhotographerData(photographerData) {
+  const photographersHeader = document.querySelector(".photograph-header");
+  const photographer = photographerFactory(photographerData);
+  const userHeaderTextDOM = photographer.getUserHeaderTextDOM();
+  photographersHeader.prepend(userHeaderTextDOM);
+  const userAvatarDOM = photographer.getUserAvatarDOM();
+  photographersHeader.appendChild(userAvatarDOM);
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const photographerData = await getPhotographerData();
+  displayPhotographerData(photographerData[0][0]);
+}
+
+init();
